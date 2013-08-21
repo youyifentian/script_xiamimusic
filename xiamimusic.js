96b3527198943a2cb153c612e3c63f816f0fe2b5
有一份田 <youyifentian@gmail.com>
2013-08-21 18:24:13 +0800

    1.0.2Beta版发布,优化数据请求逻辑,由服务器自动分配更快的地址

--- xiamimusic.js
+++ xiamimusic.js
@@ -10,57 +10,72 @@
 // @encoding	utf-8
 // @include	http://www.xiami.com/download/*
 // @run-at	document-end
-// @version	1.0.1
+// @version	1.0.2
 // ==/UserScript==
-
 
 
 /*
  * === 说明 ===
+ *
  *@作者:有一份田
  *@官网:http://duoluohua.com/download/
  *@Email:youyifentian@gmail.com
  *@Git:http://git.oschina.net/youyifentian
  *@转载重用请保留此信息
- *@最后修改时间:2013.08.19
+ *@最后修改时间:2013.08.21
  *
+ * === /说明 ===
  * */
 
 
+const version="1.0.2";
 
-const version="1.0.1";
-
-//该值决定了当检测到您不是虾米VIP会员时向远程服务器请求数据;
+//该值决定了当检测到您不是虾米VIP会员时是否会向远程服务器请求数据;
 const isRemote=true;
 
-//该值为远程服务器地址,该值允许用户自定义远程服务器,远程服务器的输出格式需为json,如:{"status":1,"location":"加密后的目标音乐url","sharename":"分享用户的名字"}
-const remoteUrl="http://duoluohua.com/myapp/api/xiami/getsong/?action=getsong&songid=";
+//该值为自定义远程服务器地址,输出格式需为json,
+//如:{"status":1,"location":"加密的目标音乐url","sharename":"分享者名字","shareurl":"分享者主页"}
+const remoteUrl="";
 
-//该值为您自定义的虾米VIP会员Cookie,和会员uid,二者必须同时提供才有效且该Cookie必须为该uid的Cookie,如无效则会返回服务器上有效的VIP数据,注意:服务器不会保存自定义Cookie,俺是您可以根据脚本提供的链接主动分享您的有效VIP会员Cookie;
+//该值为您自定义的虾米VIP会员Cookie,和会员uid,
+//二者必须同时提供才有效且该Cookie必须为该uid的Cookie,
+//如无效则会返回服务器上有效的VIP数据
+//注意:服务器不会保存自定义Cookie,但是您可以根据脚本提供的链接主动分享您的有效VIP会员Cookie;
 const locaCookie="";
 const locaUid="";
 
 
-var songs=(function(){
-	var songs=[],objs=document.getElementsByClassName("song_info");
+var songsList=(function(){
+	var arr=[],objs=document.getElementsByClassName("song_info");
 	for(var i=1;i<objs.length;i++){
 		var tmpObj=objs[i].childNodes[1];
 		var song=getSongInfo(tmpObj);
-		if(song)songs.push(song);
+		if(song)arr.push(song);
 	}
-	return songs;
-})(),uid=getUid(),shareurl="http://duoluohua.com/myapp/api/xiami/share/?action=share",
-sharelisturl="http://duoluohua.com/myapp/api/xiami/share/?action=sharelist";
-if(uid){
-	var url="http://www.xiami.com/vip/update-tone?user_id="+uid+"&tone_type=1";
+	return arr;
+})(),uid=getUid(),
+    shareurl="http://duoluohua.com/myapp/api/xiami/share/?action=share",nowQuerySongUrl="",
+    sharelisturl="http://duoluohua.com/myapp/api/xiami/share/?action=sharelist",
+    locaQueryData="locacookie="+locaCookie+"&locauid="+locaUid,
+    queryUrl="http://duoluohua.com/myapp/api/xiami/getsong/?action=geturl&fromid=xiamimusicscript&version="+version,
+    msgArr=[
+	'您还 <font color="#FF55AA">不是</font> 虾米VIP会员,但您可以尝试修改脚本中 isRemote 的值来获取远程数据,以获取高品质音乐',
+	'您是虾米VIP会员,您可以直接下载高品质音乐,您也可以 <a style="text-decoration:underline;" href="javascript:void(0);" onclick=\'window.open("'+shareurl+'");\' >点此</a> 分享您的Cookie----人人喂我,我喂人人<div><a style="text-decoration:underline;color:#999999;" href="javascript:void(0);" onclick=\'window.open("'+sharelisturl+'");\'>贡献者名单</a></div>',
+	'您使用了自定义的VIP会员Cookie'+(remoteUrl.length ? "和自定义的远程服务器" : "")+',正在为您请求数据...',
+	'您请求了远程VIP会员的数据,正在为您加载中...'
+	];
+(function(){
+	if(!uid)return;
+	var url="http://www.xiami.com/vip/update-tone?tone_type=1&user_id="+uid;
 	showUerInfo();
 	myAjax(url,function(opt){
-		var type=opt.status,
-		    text=type==1 ? '您是虾米VIP会员,您可以直接下载高品质音乐,您也可以 <a style="text-decoration:underline;" href="javascript:void(0);" onclick=\'window.open("'+shareurl+'");\' >点此</a> 分享您的Cookie----人人喂我,我喂人人<div><a style="text-decoration:underline;color:#999999;" href="javascript:void(0);" onclick=\'window.open("'+sharelisturl+'");\'>贡献者名单</a></div>' : (isRemote ? ((locaCookie.length && locaUid) ? "您使用了自定义的VIP会员Cookie,正在为您请求数据..." :"您请求了远程VIP会员的数据,正在为您加载中...") : "您还 <font color='#FF55AA'>不是</font> 虾米VIP会员,但您可以尝试修改脚本中 isRemote 的值来获取远程数据,以获取高品质音乐");
-		showUerInfo(text);
-		makeDownHtml(type);
+		var type=opt.status;
+		type=type==1 ? 1 : isRemote ? ((locaCookie && locaUid) ? 2 : 3) : 0;
+		showUerInfo(msgArr[type]);
+		getQueryUrl(type);
 	});
-}
+})();
+
 function getUid(){
 	var o=document.getElementsByClassName("uico_lbry")[0];
 	var reg=new RegExp("u\\/(\\w*)","ig");
@@ -72,51 +87,82 @@ function getSongInfo(o){
 	var arr=reg.exec(o.href);
 	return {"o":o,"id":arr[1] || ""};
 }
-function makeDownHtml(type){
-	for(var i=0;i<songs.length;i++){
-		var song=songs[i];
-		querySong(song.o,song.id,type);
+
+//1.如果是VIP会员则直接请求
+//2.如果自定义了正确的远程信息,则访问远程服务器
+//3.先访问作者服务器,由服务器给出最快的远程服务器地址,再开始请求
+
+function getQueryUrl(type){
+	var url="http://www.xiami.com/song/gethqsong/sid/";
+	if(type<2){
+		nowQuerySongUrl=url
+	}else{
+		if(remoteUrl.length){
+			nowQuerySongUrl=buildUri(remoteUrl,locaQueryData);
+		}else{
+			GM_xmlhttpRequest({
+				method: "GET",
+				url: queryUrl,
+				onload: function(response) {
+					var data=response.responseText,opt=JSON.parse(data);
+					nowQuerySongUrl=url;
+					if(opt.msg)showUerInfo(opt.msg);
+					if(opt.status)nowQuerySongUrl=buildUri(opt.url,locaQueryData);
+					goQuerySong();
+				}
+			});
+		}
 	}
+	if(nowQuerySongUrl)goQuerySong();
+}
+function goQuerySong(){
+	for(var i=0;i<songsList.length;i++){
+		var song=songsList[i];
+		querySong(song.o,song.id);
+	}	
+}
+function querySong(o,id){
+	var url=nowQuerySongUrl+id;
+	
+	showSongsInfo(o);
+	myAjax(url,function(opt){
+		opt.location=deStr(opt.location);
+		showSongsInfo(o,opt);
+	});
 }
 function showUerInfo(text){
-	var o=document.getElementById("song_count").parentNode.parentNode;
-	var html='<div align="center" style="color:#008000;"><b>'+(text || "用户数据赶来中...")+'</b></div>',
+	var o=document.getElementById("song_count").parentNode.parentNode,
+	    html='<div align="center" style="color:#008000;"><b>'+(text || "用户数据赶来中...")+'</b></div>',
 	    node=this.node;
 	if(node){
 		node.innerHTML=html;
-	}else{
-		node=document.createElement("span");
-		imgupdate=document.createElement("div");
-		imgupdate.innerHTML='<div align="center" ><a target="_blank" href="http://duoluohua.com/download/?fromid=xiami_music_script"><img id="updateimg" style="display:none;"/></a></div>';
-		node.innerHTML=html;
-		o.appendChild(node);
-		o.appendChild(imgupdate);
-		this.node=node;
-		checkUpdate();
+		return;
 	}
+	node=document.createElement("span");
+	updateNode=document.createElement("div");
+	updateNode.innerHTML='<div align="center" ><a target="_blank" href="http://duoluohua.com/download/?fromid=xiami_music_script"><img id="updateimg" style="display:none;"/></a></div>';
+	node.innerHTML=html;
+	o.appendChild(node);
+	o.appendChild(updateNode);
+	this.node=node;
+	checkUpdate();
 }
-function showSongsInfo(o,opt,type){
+function showSongsInfo(o,opt){
 	var node=o.node,url=opt ? opt.location : "";
 	if(node){
 		node.innerHTML='<a href="javascript:void(0);" onclick=\'window.open("'+url+'");\' style="float:right;position:relative;margin-right:30px;text-decoration:underline;">点此下载</a>';
-	}else{
-		var node=document.createElement("span");
-		node.innerHTML='<span style="float:right;position:relative;margin-right:10px;color:#A1CBE4;">数据正在赶来中...</span>';
-		o.parentNode.appendChild(node);
-		o.node=node;
+		if(opt && opt.msg)showUerInfo(opt.msg);
+		return;
 	}
-	if(opt && opt.msg)showUerInfo(opt.msg);
+	var node=document.createElement("span");
+	node.innerHTML='<span style="float:right;position:relative;margin-right:10px;color:#A1CBE4;">数据正在赶来中...</span>';
+	o.parentNode.appendChild(node);
+	o.node=node;	
 }
-function querySong(o,id,type){
-	var url="http://www.xiami.com/song/gethqsong/sid/"+id;
-	if(type!=1 && isRemote){
-		url=remoteUrl+id+"&locacookie="+locaCookie+"&locauid="+locaUid;
-	}
-	showSongsInfo(o)
-	myAjax(url,function(opt){
-		opt.location=deStr(opt.location);
-		showSongsInfo(o,opt,type);
-	});
+function buildUri(url,strData){
+	var arr_1=url.split("?"),arr_2=strData.split("&");
+	var path=arr_1[0],tmp=arr_1[1] ? arr_1[1].split("&") : [];
+	return path+"?"+tmp.concat(arr_2).join("&")+"&songid=";
 }
 function myAjax(url,callback){
 	GM_xmlhttpRequest({
@@ -135,6 +181,13 @@ function checkUpdate(){
 	js+='info.style.display="block";';
 	js+='}';
 	loadJs(js);
+}
+function loadJs(js){
+	var oHead=document.getElementsByTagName('HEAD')[0],
+	    oScript= document.createElement("script"); 
+	oScript.type = "text/javascript"; 
+	oScript.text =js;
+	oHead.appendChild( oScript); 	
 }
 function deStr(str){
 	var _loc_10=undefined;
@@ -179,13 +232,6 @@ function deStr(str){
 	_loc_9 = _loc_9.replace("+", " ");
 	return _loc_9;
 }
-function loadJs(js){
-	var oHead=document.getElementsByTagName('HEAD')[0],
-	    oScript= document.createElement("script"); 
-	oScript.type = "text/javascript"; 
-	oScript.text =js;
-	oHead.appendChild( oScript); 	
-}
 function googleAnalytics(){
 	var js="var _gaq = _gaq || [];";
 	js+="_gaq.push(['_setAccount', 'UA-43280861-1']);";
@@ -201,4 +247,3 @@ function googleAnalytics(){
 	loadJs(js);
 }
 googleAnalytics();
-
